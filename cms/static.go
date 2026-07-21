7b5149d2b8560a95ab62d2e1cbdaf36b2a1af765
Meduzz <drgevalia@gmail.com>
2026-07-21 16:46:18 +0200

    A few improvements to static

--- cms/static.go
+++ cms/static.go
@@ -12,17 +12,19 @@ type (
 		Children   []*Static `json:"children,omitempty"`
 		Attributes []string  `json:"attributes,omitempty"`
 	}
+
+	rawHtml struct {
+		html string
+	}
 )
 
 func Render(data *Static) string {
-	return transform(data).Render()
+	return Transform(data).Render()
 }
 
-func transform(in *Static) gml.Tag {
+func Transform(in *Static) gml.Tag {
 	if len(in.Children) > 0 {
-		children := slice.Map(in.Children, func(it *Static) gml.Tag {
-			return transform(it)
-		})
+		children := slice.Map(in.Children, Transform)
 
 		return gml.New(in.Tag, gml.Tags(children...), gml.Attributes(in.Attributes...))
 	} else if in.Text != "" {
@@ -30,4 +32,15 @@ func transform(in *Static) gml.Tag {
 	} else {
 		return gml.New(in.Tag, gml.Empty(), gml.Attributes(in.Attributes...))
 	}
+}
+
+// RawHtml - obviously bad, but also sometimes needed. Use for every day adventures.
+func RawHtml(html string) gml.Tag {
+	return &rawHtml{
+		html: html,
+	}
+}
+
+func (r *rawHtml) Render() string {
+	return r.html
 }
